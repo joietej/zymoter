078c4d0b94b4308c23035da04588c827bb180ea8
Tejas Sahasrabudehe <tejas.sahasrabudhe@gmail.com>
2020-12-21 08:04:40 +0100

    make it pwa : manifest & icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,25 @@
 import Layout from "../layout/Layout";
 import "../styles/app.scss";
 
+import Head from "next/Head";
+
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta charset="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="sample store app" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <title>Zymoter</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 };
 
