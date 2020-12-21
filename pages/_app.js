d62dbc93f4399993520adc6918a66652b90baa24
Tejas Sahasrabudehe <tejas.sahasrabudhe@gmail.com>
2020-12-21 10:15:38 +0100

    make it pwa 2 - service worker

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,14 @@
+import Head from "next/head";
+import useServiceWorker from "../hooks/serviceWorker";
 import Layout from "../layout/Layout";
 import "../styles/app.scss";
 
-import Head from "next/head";
-
 const MyApp = ({ Component, pageProps }) => {
+  useServiceWorker();
   return (
     <>
       <Head>
-        <meta charset="utf-8" />
-        <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#000000" />
-        <meta name="description" content="sample store app" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <title>Zymoter</title>
       </Head>
       <Layout>
         <Component {...pageProps} />
