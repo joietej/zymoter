8e7a300201c7e77c61398a5b1072116f2fb56f52
Tejas Sahasrabudehe <tejas.sahasrabudhe@gmail.com>
2021-01-10 21:03:36 +0100

    Shipping form - 1

--- layout/checkout/Shipping.js
+++ layout/checkout/Shipping.js
@@ -1,12 +1,56 @@
 import React from "react";
 import { Form, FormGroup, Select, TextInput } from "carbon-components-react";
 
+const defaultOrder = {
+  shipping: {
+    firstname: "",
+    lastname: "",
+    email: "",
+    address: {
+      street: "",
+      city: "",
+      zip: "",
+      country: "",
+      state: "",
+    },
+    method: "",
+  },
+};
+
 const Shipping = () => {
+  const [order, setOrder] = React.useState(defaultOrder);
+
+  const onChange = (e) => {
+    e.preventDefault();
+    const new_order = { ...order };
+    new_order.shipping[e.target.name] = e.target.value;
+    setOrder(new_order);
+  };
+
   return (
     <Form>
       <FormGroup legendText="Personal">
-        <TextInput labelText="Full Name" required />
-        <TextInput type="email" labelText="Email" required />
+        <TextInput
+          labelText="First Name"
+          value={order.shipping.firstname}
+          required
+          onChange={onChange}
+          name="firstname"
+        />
+        <TextInput
+          labelText="Last Name"
+          value={order.shipping.lastname}
+          required
+          onChange={onChange}
+          name="lastname"
+        />
+        <TextInput
+          type="email"
+          labelText="Email"
+          required
+          name="email"
+          onChange={onChange}
+        />
       </FormGroup>
 
       <FormGroup legendText="Address">
